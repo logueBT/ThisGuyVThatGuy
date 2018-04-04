8c3cbb7221f087d024d8d3a2f71aa27894279934
Logue,J,Joshua,TAW6 R <joshua.logue@bt.com>
2018-04-04 16:14:52 +0100

    started on ui tests

--- ThisGuyVThatGuyUITest/Tests.cs
+++ ThisGuyVThatGuyUITest/Tests.cs
@@ -17,12 +17,6 @@ namespace ThisGuyVThatGuyUITest
             this.platform = platform;
         }
 
-        [Test]
-        public void AppLaunches()
-        {
-            app.Screenshot("First screen.");
-        }
-
         /// <summary>
         /// Test to open the REPL window
         /// </summary>
@@ -30,6 +24,78 @@ namespace ThisGuyVThatGuyUITest
         public void RunRepl()
         {
             this.app.Repl();
+        }
+
+        /// <summary>
+        /// Test the opening screen
+        /// </summary>
+        [Test]
+        public void TestFirstScreen()
+        {
+            this.app.WaitForElement("pageTitle");
+            this.app.WaitForElement("pageInstructions");
+            this.app.WaitForElement("pickerExplainer");
+            this.app.WaitForElement("numberPicker");
+            this.app.WaitForElement("numberCorrectLabel");
+            this.app.WaitForElement("numberCorrectCount");
+            this.app.WaitForElement("successMessage");
+            this.app.WaitForElement("goButton");
+            this.app.WaitForNoElement("playerListView");
+
+            
+            Assert.AreEqual(this.app.Query("pageTitle").First().Text, "This Guy V That Guy", "title text not right");
+            Assert.AreEqual(this.app.Query("pageInstructions").First().Text, "Pick which player has the largest FPPG, get 10 right and you win", "instructions text not right");
+            Assert.AreEqual(this.app.Query("pickerExplainer").First().Text, "How many players do you want to choose from?", "picker text not right");
+            Assert.AreEqual(this.app.Query("numberCorrectLabel").First().Text, "Number correct:", "Number correct text not right");
+            Assert.AreEqual(this.app.Query("numberCorrectCount").First().Text, "0", "count not right");
+            Assert.AreEqual(this.app.Query("successMessage").First().Text, "Pick who has the highest FPPG", "success text not right");
+            Assert.AreEqual(this.app.Query("goButton").First().Text, "Go!", "button label not right");
+            Assert.AreEqual(this.app.Query("numberPicker").First().Text, "2", "picker label not right");
+        }
+
+        /// <summary>
+        /// Start game with 2 players, select first player
+        /// </summary>
+        [Test]
+        public void TestStartGame()
+        {
+            this.app.WaitForElement("goButton");
+            this.app.WaitForNoElement("playerListView");
+
+            this.app.Tap("goButton");
+
+            this.app.WaitForElement("playerListView");
+
+            int count = this.app.Query("playerListViewName").Count();
+            int countPicker = Int32.Parse(this.app.Query("numberPicker").First().Text);
+
+            Assert.AreEqual(count, countPicker, "not correct length of list");
+
+            this.app.WaitForNoElement("playerListViewFppg");
+
+            var elements = this.app.Query(c => c.Marked("playerListViewName"));
+
+            this.app.TapCoordinates(elements[0].Rect.CenterX, elements[0].Rect.CenterY);
+
+            this.app.WaitForElement("playerListViewFppg");
+
+            var fppgElements = this.app.Query(c => c.Marked("playerListViewFppg"));
+            double first = Convert.ToDouble(fppgElements[0].Text);
+            double second = Convert.ToDouble(fppgElements[1].Text);
+
+            if (first > second)
+            {
+                this.app.WaitForElement("Correct, keep going");
+                Assert.AreEqual(this.app.Query("numberCorrectCount").First().Text, "1", "count not right");
+                Assert.AreEqual(this.app.Query("successMessage").First().Text, "Correct, keep going", "success text not right");
+                Assert.AreEqual(this.app.Query("goButton").First().Text, "Go again", "button label not right");
+            }
+            else
+            {
+                Assert.AreEqual(this.app.Query("numberCorrectCount").First().Text, "0", "count not right");
+                Assert.AreEqual(this.app.Query("successMessage").First().Text, "Wrong, try again", "success text not right");
+                Assert.AreEqual(this.app.Query("goButton").First().Text, "Try again", "button label not right");
+            }
         }
     }
 }
